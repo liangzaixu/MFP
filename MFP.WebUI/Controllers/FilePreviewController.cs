b6d120c5fa6bdcbd0ebf71e7ce24423d32864c14
liangzaixu <280681432@qq.com>
2019-03-14 17:57:12 +0800

    filePreview demo

---
+++ MFP.WebUI/Controllers/FilePreviewController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MFP.WebUI.Controllers
+{
+    public class FilePreviewController : Controller
+    {
+        // GET: FilePreview
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OfficeFile()
+        {
+            return View();
+        }
+
+        public JsonResult ConvertToPdf(string filePath)
+        {
+
+            return Json("");
+        }
+
+        public ActionResult TxtFile()
+        {
+            return View();
+        }
+
+        public ActionResult VideoFile()
+        {
+            return View();
+        }
+
+        public ActionResult AudioFile()
+        {
+            return View();
+        }
+
+        public ActionResult PDF()
+        {
+            return View();
+        }
+    }
+}
